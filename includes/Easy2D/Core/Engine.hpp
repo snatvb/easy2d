a6c5d09fdbc15dfad8a44d80d4d86de3601f84a8
Andrey Avsenin <a_avsenin@wargaming.net>
2021-05-01 22:44:50 +0300

    Added SDL

--- includes/Easy2D/Core/Engine.hpp
+++ includes/Easy2D/Core/Engine.hpp
@@ -1,5 +1,19 @@
 #pragma once
 
-namespace easy2d {
-    int checking();
-}
+namespace easy2d
+{
+    class Engine;
+
+    Engine &getEngine();
+
+    class Engine
+    {
+    public:
+        void initialize();
+
+    private:
+        Engine() = default;
+        friend Engine &getEngine();
+    };
+
+} // -- easy2d
